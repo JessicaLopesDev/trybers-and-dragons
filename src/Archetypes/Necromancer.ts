c9ed10b7ed759a0beb105b41ed3854b0520c5bbd
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-07-10 19:27:14 -0300

    Requisito 5 ok

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Necromancer extends Archetype {
+  private _energyType: EnergyType;
+  private static _necromancerInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Necromancer._necromancerInstances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._necromancerInstances;
+  }
+}
+
+export default Necromancer;
